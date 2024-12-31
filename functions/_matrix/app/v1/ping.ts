44d1e0b672d1f1b8ea968c7367c682781c4cbc54
AsenHu <78863300+AsenHu@users.noreply.github.com>
2024-12-31 06:33:28 +0000

    修复 sha512 缺少 await，加入大量日志

--- functions/_matrix/app/v1/ping.ts
+++ functions/_matrix/app/v1/ping.ts
@@ -11,26 +11,35 @@ const onRequestPost = async (context: Context) => {
     // 鉴权
     const config = JSON.parse(context.env.config);
     const auth = context.request.headers.get('Authorization');
+    console.log("check auth");
     if (auth !== `Bearer ${config.App_Service.hs_token}`) {
+        console.log("auth failed");
         return return_403();
     }
 
     // 获取请求
     const body: Body = await context.request.json();
+    console.log("accept request", body);
     // body.transaction_id 应该为 "Im_so_sleepy-" + 时间戳 + "-" + 签名
     // 检查时间戳，不应超过 1 分钟
+    console.log("date now", Date.now());
     const timestamp = parseInt(body.transaction_id.split('-')[1]);
     if (Date.now() - timestamp > 60000) {
+        console.log("timestamp failed", Date.now() - timestamp);
         return return_403();
     }
     // 签名为 sha512("Im_so_sleepy-" + 时间戳 + "-" + config.seed)
     // 检查签名
-    const sign = body.transaction_id.split('-')[2];
-    if (sign !== sha512("Im_so_sleepy-" + timestamp + "-" + config.seed).toString()) {
+    const sign = body.transaction_id.split('-')[2].toLowerCase();
+    const sign_check = (await sha512("Im_so_sleepy-" + timestamp + "-" + config.seed)).toString().toLowerCase();
+    console.log("sign", sign, sign_check);
+    if (sign !== sign_check) {
+        console.log("sign failed");
         return return_403();
     }
 
     // 返回
+    console.log("return 200");
     return return_200();
 }
 
